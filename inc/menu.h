cd4282d7fafd0dce9e8eda62415a063b367e0c56
alemantus <Alexander@anak.dk>
2018-06-11 11:02:10 +0200

    test

---
+++ inc/menu.h
@@ -0,0 +1,10 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+#include "stm32f30x_conf.h"
+#include <stdio.h>
+#include <string.h>
+#include "charset.h"
+#include "30010_io.h"
+
+#endif
---
+++ src/menu.c
@@ -0,0 +1,7 @@
+#include "menu.h"
+#include "ansi.h"
+
+
+void menu1(){
+
+}
