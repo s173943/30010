1cdabfa8bbc9858b17a2514d0fecf6fa27372d4c
alemantus <Alexander@anak.dk>
2018-06-18 12:31:52 +0200

    ting

--- src/menu.c
+++ src/menu.c
@@ -5,56 +5,52 @@
 #include "lcd.h"
 extern uint8_t updateLCD;
 void drawSonicLoser(uint8_t playingField[128][32],uint8_t oldPlayingField[128][32], uint8_t *sonicCount){
-    if (*sonicCount == 20){
+    if (*sonicCount == 10){
         memset(playingField, 0x00, sizeof (uint8_t) * 128 * 32);
         sonicAni1(playingField, 0,0);
         drawChangeInArray(playingField, oldPlayingField);
         convertArrayToBuffer(playingField);
         lcd_push_buffer(lcdArray);
     }
-    else if(*sonicCount == 40){
+    else if(*sonicCount == 20){
         memset(playingField, 0x00, sizeof (uint8_t) * 128 * 32);
         sonicAni2(playingField, 0,0);
         drawChangeInArray(playingField, oldPlayingField);
         convertArrayToBuffer(playingField);
         lcd_push_buffer(lcdArray);
     }
-    else if(*sonicCount == 60){
+    else if(*sonicCount == 30){
         memset(playingField, 0x00, sizeof (uint8_t) * 128 * 32);
         sonicAni3(playingField, 0,0);
         drawChangeInArray(playingField, oldPlayingField);
         convertArrayToBuffer(playingField);
         lcd_push_buffer(lcdArray);
     }
-    else if (*sonicCount == 80){
-        *sonicCount = 0;
-    }
+
 }
 void drawSonicWinner(uint8_t playingField[128][32],uint8_t oldPlayingField[128][32], uint8_t *sonicCount){
-    if (*sonicCount == 20){
+    if (*sonicCount == 10){
         memset(playingField, 0x00, sizeof (uint8_t) * 128 * 32);
-        sonicAni1(playingField, 0,0);
+        sonicAni1(playingField, 50,0);
         drawChangeInArray(playingField, oldPlayingField);
         convertArrayToBuffer(playingField);
         lcd_push_buffer(lcdArray);
     }
-    else if(*sonicCount == 40){
+    else if(*sonicCount == 20){
         memset(playingField, 0x00, sizeof (uint8_t) * 128 * 32);
-        sonicAni2(playingField, 0,0);
+        sonicAni2(playingField, 50,0);
         drawChangeInArray(playingField, oldPlayingField);
         convertArrayToBuffer(playingField);
         lcd_push_buffer(lcdArray);
     }
-    else if(*sonicCount == 60){
+    else if(*sonicCount == 30){
         memset(playingField, 0x00, sizeof (uint8_t) * 128 * 32);
-        sonicAni4(playingField, 0,0);
+        sonicAni4(playingField, 50,0);
         drawChangeInArray(playingField, oldPlayingField);
         convertArrayToBuffer(playingField);
         lcd_push_buffer(lcdArray);
     }
-    else if (*sonicCount == 80){
-        *sonicCount = 0;
-    }
+
 }
 void menuTree(uint8_t playingField[128][32], uint8_t oldPlayingField[128][32],int8_t *menuSettings, uint16_t *testCount){
     uint8_t x, oldx, menuTrack = 0;
