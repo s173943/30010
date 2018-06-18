43f4338e2927d2ae4050ac0df9a539f989cf9a37
s173943 <40024706+s173943@users.noreply.github.com>
2018-06-18 17:17:55 +0200

    plz

--- inc/ansi.h
+++ inc/ansi.h
@@ -18,7 +18,7 @@ void underline(uint8_t on);
 void inverse(uint8_t on);
 void showCursor(uint8_t on);
 void blink(uint8_t on);
-void window(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, char name[], uint8_t style);
+//void window(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, char name[], uint8_t style);
 void drawWindowFromArray(uint8_t playingField[128][32]);
 void drawChangeInArray(uint8_t playingField[128][32], uint8_t oldPlayingField[128][32]);
 
